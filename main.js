350087fd0d24b4401902fd2470157d21e06f97a6
DanielEchiNg <112507048+DanielCoded@users.noreply.github.com>
2024-11-13 00:22:21 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDlS7-WKloQq0_p7sIJ6Gfh0pw3z86AHmw",
+authDomain: "tefipay-ba888.firebaseapp.com",
+projectId: "tefipay-ba888",
+storageBucket: "tefipay-ba888.firebasestorage.app",
+messagingSenderId: "801163667947",
+appId: "1:801163667947:web:9d8b7d3a685842973bd1ae",
+measurementId: "G-YTXNJW4SEF"
+};
+
